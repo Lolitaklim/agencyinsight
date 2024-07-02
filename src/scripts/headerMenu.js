75ba8c570212a1171a021bb9b493fc5219db9c37
Lolitaklim <115589478+Lolitaklim@users.noreply.github.com>
2024-07-03 01:23:40 +0400

    upd

--- src/scripts/headerMenu.js
+++ src/scripts/headerMenu.js
@@ -1,10 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const toggleButton = document.getElementById('toggleMenu')
+  const toggleMenuButton = document.getElementById('toggleMenu')
   const panelRight = document.getElementById('panelRight')
   const panelLeft = document.getElementById('panelLeft')
+  toggleMenuButton.addEventListener('click', function () {
+    panelRight.classList.toggle('opened')
+    panelLeft.classList.toggle('opened')
+    toggleMenuButton.classList.toggle('close-btn')
+  })
 
-  toggleButton.addEventListener('click', function () {
-    panelRight.classList.toggle('opened-right')
-    panelLeft.classList.toggle('opened-left')
+  let buttonsFillHover = document.querySelectorAll('.button-fill-hover')
+  buttonsFillHover.forEach((button) => {
+    button.addEventListener('mousemove', (e) => {
+      x = e.offsetX
+      y = e.offsetY
+      button.style.setProperty('--mouse-x', x + 'px')
+      button.style.setProperty('--mouse-y', y + 'px')
+    })
+  })
+
+  const toggleButton = document.getElementById('dropdown-toggle')
+  const dropdownMenu = document.getElementById('dropdown-menu')
+  const dropdownContent = document.getElementById('dropdown-content')
+  const chevron = document.getElementById('chevron')
+  toggleButton.addEventListener('click', function (event) {
+    event.preventDefault()
+    if (dropdownMenu.classList.contains('open')) {
+      dropdownContent.style.maxHeight = null
+    } else {
+      dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px'
+    }
+    dropdownMenu.classList.toggle('open')
+    chevron.classList.toggle('open-span')
   })
 })
