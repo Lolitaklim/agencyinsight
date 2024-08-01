3ccf935f04a456cc7231459d557d668bf9253a86
Lolitaklim <115589478+Lolitaklim@users.noreply.github.com>
2024-08-01 09:43:30 +0400

    menu

--- frontend/src/scripts/footer.js
+++ frontend/src/scripts/footer.js
@@ -1,11 +1,13 @@
-const openFormMobileBtn = document.getElementById('openFormMobileBtn')
-const formContainerMobile = document.getElementById('formLeaving')
+const openFormLeavingButtons = document.querySelectorAll('.openFormLeavingBtn')
+const formLeaving = document.getElementById('formLeaving')
 const closeFormMobileBtn = document.getElementById('closeFormLeavingBtn')
 
-openFormMobileBtn.addEventListener('click', function () {
-  formContainerMobile.classList.add('show')
+openFormLeavingButtons.forEach((button) => {
+  button.addEventListener('click', function () {
+    formLeaving.classList.add('show')
+  })
 })
 
 closeFormMobileBtn.addEventListener('click', function () {
-  formContainerMobile.classList.remove('show')
+  formLeaving.classList.remove('show')
 })
--- frontend/src/scripts/headerMenu.js
+++ frontend/src/scripts/headerMenu.js
@@ -1,35 +1,59 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggleMenuButton = document.getElementById('toggleMenu')
-  const panelRight = document.getElementById('panelRight')
-  const panelLeft = document.getElementById('panelLeft')
-  toggleMenuButton.addEventListener('click', function () {
-    panelRight.classList.toggle('opened')
-    panelLeft.classList.toggle('opened')
-    toggleMenuButton.classList.toggle('close-btn')
-  })
+// document.addEventListener('DOMContentLoaded', function () {
+const openMenu = document.getElementById('openMenu')
+const closeMenu = document.getElementById('closeMenu')
 
-  let buttonsFillHover = document.querySelectorAll('.button-fill-hover')
-  buttonsFillHover.forEach((button) => {
-    button.addEventListener('mousemove', (e) => {
-      x = e.offsetX
-      y = e.offsetY
-      button.style.setProperty('--mouse-x', x + 'px')
-      button.style.setProperty('--mouse-y', y + 'px')
-    })
-  })
+const panelRight = document.getElementById('panelRight')
+const panelLeft = document.getElementById('panelLeft')
+openMenu.addEventListener('click', function () {
+  panelRight.classList.toggle('opened')
+  panelLeft.classList.toggle('opened')
+})
+closeMenu.addEventListener('click', function () {
+  panelRight.classList.toggle('opened')
+  panelLeft.classList.toggle('opened')
+})
 
-  // const toggleButton = document.getElementById('dropdown-toggle')
-  // const dropdownMenu = document.getElementById('dropdown-menu')
-  // const dropdownContent = document.getElementById('dropdown-content')
-  // const chevron = document.getElementById('chevron')
-  // toggleButton.addEventListener('click', function (event) {
-  //   event.preventDefault()
-  //   if (dropdownMenu.classList.contains('open')) {
-  //     dropdownContent.style.maxHeight = null
-  //   } else {
-  //     dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px'
-  //   }
-  //   dropdownMenu.classList.toggle('open')
-  //   chevron.classList.toggle('open-span')
-  // })
+// let buttonsFillHover = document.querySelectorAll('.button-fill-hover')
+// buttonsFillHover.forEach((button) => {
+//   button.addEventListener('mousemove', (e) => {
+//     x = e.offsetX
+//     y = e.offsetY
+//     button.style.setProperty('--mouse-x', x + 'px')
+//     button.style.setProperty('--mouse-y', y + 'px')
+//   })
+// })
+
+// const toggleButton = document.getElementById('dropdown-toggle')
+// const dropdownMenu = document.getElementById('dropdown-menu')
+// const dropdownContent = document.getElementById('dropdown-content')
+// const chevron = document.getElementById('chevron')
+// toggleButton.addEventListener('click', function (event) {
+//   event.preventDefault()
+//   if (dropdownMenu.classList.contains('open')) {
+//     dropdownContent.style.maxHeight = null
+//   } else {
+//     dropdownContent.style.maxHeight = dropdownContent.scrollHeight + 'px'
+//   }
+//   dropdownMenu.classList.toggle('open')
+//   chevron.classList.toggle('open-span')
+// })
+// })
+
+const buttonChevron = document.getElementById('buttonDropdownMenu')
+
+buttonChevron.addEventListener('click', () => {
+  const content = document.getElementById('dropdownContentMenu')
+  const chevron = document.getElementById('chevronMenu')
+  const informationMenu = document.getElementById('informationMenu')
+
+  const expanded = chevron.classList.toggle('expanded')
+  if (window.innerHeight / window.innerWidth < 2 && window.innerWidth < 650) {
+    informationMenu.classList.toggle('display')
+  }
+
+  if (expanded) {
+    content.style.maxHeight = content.scrollHeight + 'px'
+  } else {
+    content.style.maxHeight = null
+  }
 })
